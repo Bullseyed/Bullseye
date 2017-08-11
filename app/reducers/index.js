4968e54d3e214adf4523b792795b4b7657fc44ef
Ryan Margono <rtm308@nyu.edu>
2017-08-11 17:36:28 -0400

    done

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
 
-
 const rootReducer = combineReducers({
-  data: require('./data-reducers').default,
   map: require('./map-reducer').default,
   rests: require('./rest-reducer').default,
 
