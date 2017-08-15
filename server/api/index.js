6aef91e76f2a8da8b007c87f1ba67f5c4a51c8a2
Danielboufford <daniel.boufford@gmail.com>
2017-08-15 19:32:21 -0400

    oauth continued...

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
-router.use('/yelp', require('./yelp'))
+router.use('/yelp', require('./yelp'));
+router.use('/auth', require('./auth'));
 
 router.use(function (req, res, next) {
   const err = new Error('Not found.');
--- server/start.js
+++ server/start.js
@@ -4,7 +4,6 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 const db = require('../db');
 
@@ -16,40 +15,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 // you'll of course want static middleware so your browser can request things like your 'bundle.js'
 app.use(express.static(path.join(__dirname, '../public/')));
 
 // Any routes or other various middlewares should go here!
 app.use('/api', require('./api')); // matches all requests to /api
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('auth/google', passport.authenticate('google', { scope: 'email' }));
-app.get('auth/google/callback', passport.authenticate('google', {
-  successRedirect: '/',
-  failureRedirect: '/'
-}));
-
-passport.use(
-  new GoogleStrategy({
-    clientID: '92045154125-jfqstsfj8rceu7m97iq4mnv529f0qjim.apps.googleusercontent.com',
-    clientSecret: 'xXfF0HaV6e3Xxexa1u_e0SpN',
-    callbackURL: 'http://localhost:1337/auth/google/callback'
-  }, (token, refreshToken, profile, done) => {
-    var info = {
-      email: profile.emails[0].value,
-    };
-    User.findOrCreate({
-      where: {googleId: profile.id},
-      defaults: info
-    })
-    .spread(user => {
-      done(null, user);
-    })
-    .catch(done);
-  })
-);
 
 // Make sure this is right at the end of your server logic!
 // The only thing after this might be a piece of middleware to serve up 500 errors for server problems
