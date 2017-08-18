f7c61a2e29df542243a973069bad0ad4411fb5b2
Danielboufford <daniel.boufford@gmail.com>
2017-08-18 16:54:41 -0400

    feat: reports save to the database

--- server/api/reports.js
+++ server/api/reports.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 
+const Report = require('../../db').models.report;
+const User = require('../../db').models.user;
+
 router.post('/', (req, res, next) => {
-  console.log('hello', req.body)
-  res.send(req.body)
+  console.log('hello', req.user)
+   Report.create(req.body)
+  .then(createdReport => {
+    return createdReport.setUser(req.user)
+  })
+  .then( createdReport => {
+        res.status(204)
+  })
+  .catch(next)
 });
 
 module.exports = router
