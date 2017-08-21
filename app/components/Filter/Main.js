f65d39d64714e38b7a99101919eb0e34b4d5e946
Danielboufford <daniel.boufford@gmail.com>
2017-08-21 12:57:50 -0400

    feat: how to use bullseye button/modal

--- app/components/Filter/Main.js
+++ app/components/Filter/Main.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import { Input, Row, Icon, Col } from 'react-materialize'
-import Radius from './Radius'
-import BusinessDropdown from './BusinessDropdown'
-import SubmitRepBut from './SubmitRepBut'
+import React from 'react';
+import { Row } from 'react-materialize';
+import Radius from './Radius';
+import BusinessDropdown from './BusinessDropdown';
+import SubmitRepBut from './SubmitRepBut';
+import HowToUseBut from './HowToUseBut';
 
-const Filter = (props) => {
+const Filter = () => {
 	return (
 		<div>
 			<Row>
+				<HowToUseBut />
 				<BusinessDropdown />
 			</Row>
 			<Radius />
 			<SubmitRepBut />
 		</div>
-	)
-}
+	);
+};
 
-export default Filter
+export default Filter;
