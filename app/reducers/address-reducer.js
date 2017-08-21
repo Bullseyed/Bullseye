cdd0ecff26c084d573f0c5496b97429b89463e1f
Boris Berdichevskiy <bberdich@gmail.com>
2017-08-21 12:36:14 -0400

    can now view neighborhood and address in report

---
+++ app/reducers/address-reducer.js
@@ -0,0 +1,33 @@
+import geocoding from 'reverse-geocoding';
+
+const GET_ADDRESS = 'GET_ADDRESS'
+
+const getAddress = (address) => ({
+  type: GET_ADDRESS,
+  address,
+})
+
+
+export const fetchAddress = (latLongObj) => dispatch => {
+  geocoding.location(latLongObj, function (err, data){
+    if (err) {
+        console.log(err);
+    } else {
+      console.log(data.results)
+      const address = data.results[0].formatted_address;
+      const neighborhood  = data.results[0].address_components[2].long_name;
+      dispatch(getAddress([address, neighborhood]));
+    }
+  });
+};
+
+function addressReducer (address = [], action) {
+  switch (action.type){
+    case GET_ADDRESS:
+      return action.address;
+    default:
+      return address;
+  }
+}
+
+export default addressReducer;
