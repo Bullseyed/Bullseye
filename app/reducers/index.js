511814ff15dce031385ec5686505c1e9468bbf38
Boris Berdichevskiy <bberdich@gmail.com>
2017-08-22 00:16:05 -0400

    fix: landing page

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -4,7 +4,6 @@ const rootReducer = combineReducers({
   map: require('./map-reducer').default,
   rests: require('./rest-reducer').default,
   radius: require('./radius-reducer').default,
-  checkboxes: require('./checkbox-reducer').default,
   bType: require('./b-type-reducer').default,
   demoData: require('./demo-reducer').default,
   currentUser: require('./auth').default,
