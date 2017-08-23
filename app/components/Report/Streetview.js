1c542f138b541aa3cbbaf86c6111ad9bc74ef1ef
Ryan Margono <rtm308@nyu.edu>
2017-08-23 17:25:21 -0400

    fixed div heights

--- app/components/Report/Streetview.js
+++ app/components/Report/Streetview.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Streetview = (props) => {
 	const location= (props.bullseye[0]+','+props.bullseye[1]).toString()
-	const url = 'https://maps.googleapis.com/maps/api/streetview?size=400x300&location='+location+'&heading=151.78&pitch=-0.76&key=AIzaSyDw4h8gKaVu8fuzvd-ypVMxTSbThRtBE04'
+	const url = 'https://maps.googleapis.com/maps/api/streetview?size=300x200&location='+location+'&heading=151.78&pitch=-0.76&key=AIzaSyDw4h8gKaVu8fuzvd-ypVMxTSbThRtBE04'
 	
 	
 	return (
