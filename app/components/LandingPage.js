76845ed0fc0536741737a8d1441bffa5d2ca63a2
Boris Berdichevskiy <bberdich@gmail.com>
2017-08-26 21:07:57 -0400

    fixed centering on landing page, improved comp listings in report view

--- app/components/LandingPage.js
+++ app/components/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Nav from './Nav/Nav'
 import { Row, Col, CardPanel, Card, CardTitle } from 'react-materialize'
 import { Link } from 'react-router-dom'
+import Center from 'react-center'
 
 class LandingPage extends Component {
 	constructor() {
@@ -38,53 +39,53 @@ class LandingPage extends Component {
 					<Nav />
 				</Row>
 
-				<Row style={{width: '75%', margin: '0 auto', paddingTop: 25}}>
-					<Col>
-							<Col l={6}>
-								<Link to='/business' >
-									<Card
-										className='landingCard'
-										onMouseEnter={() => this.handleMouseEnter()}
-										onMouseLeave={() => this.handleMouseLeave()}
-										header={
-											<CardTitle
-												image='BizCard.png'
-												waves='light'
-												style={{ height: 300, width: '100%', opacity: this.state.hovered ? '.4' : '1'}}
-											/>
-										}
-									>
-										{ this.state.hovered
-											? <p style={{ fontSize:16, paddingTop: '12px' }} className='center-align'>{this.state.hoverText}</p>
-											: <p style={{ fontSize:18, paddingTop: '20px' }} className='center-align'>OPEN A BUSINESS</p>
-										}
-									</Card>
-								</Link>
-							</Col>
+				<Center>
+					<Row>
+						<Col l={6}>
+							<Link to='/business' >
+								<Card
+									className='landingCard'
+									onMouseEnter={() => this.handleMouseEnter()}
+									onMouseLeave={() => this.handleMouseLeave()}
+									header={
+										<CardTitle
+											image='BizCard.png'
+											waves='light'
+											style={{ height: 300, width: '100%', opacity: this.state.hovered ? '.4' : '1'}}
+										/>
+									}
+								>
+									{ this.state.hovered
+										? <p style={{ fontSize:16, paddingTop: '12px' }} className='center-align'>{this.state.hoverText}</p>
+										: <p style={{ fontSize:18, paddingTop: '20px' }} className='center-align'>OPEN A BUSINESS</p>
+									}
+								</Card>
+							</Link>
+						</Col>
 
-							<Col l={6}>
-								<Link to='/threads'>
-									<Card
-										className='landingCard'
-										onMouseEnter={() => this.handleMouseEnter2()}
-										onMouseLeave={() => this.handleMouseLeave()}
-										header={
-											<CardTitle
-												image='ResCard.png'
-												waves='light'
-												style={{ height: 300, width: '100%', opacity: this.state.hovered2 ? '.4' : '1'}}
-											/>
-										}
-									>
-										{ this.state.hovered2
-											? <p style={{ fontSize:16, paddingTop: '12px' }} className='center-align'>{this.state.hoverText}</p>
-											: <p style={{ fontSize:18, paddingTop: '20px' }} className='center-align'>SUGGEST A BUSINESS</p>
-										}
-									</Card>
-								</Link>
-							</Col>
-					</Col>
-				</Row>
+						<Col l={6}>
+							<Link to='/threads'>
+								<Card
+									className='landingCard'
+									onMouseEnter={() => this.handleMouseEnter2()}
+									onMouseLeave={() => this.handleMouseLeave()}
+									header={
+										<CardTitle
+											image='ResCard.png'
+											waves='light'
+											style={{ height: 300, width: '100%', opacity: this.state.hovered2 ? '.4' : '1'}}
+										/>
+									}
+								>
+									{ this.state.hovered2
+										? <p style={{ fontSize:16, paddingTop: '12px' }} className='center-align'>{this.state.hoverText}</p>
+										: <p style={{ fontSize:18, paddingTop: '20px' }} className='center-align'>SUGGEST A BUSINESS</p>
+									}
+								</Card>
+							</Link>
+						</Col>
+					</Row>
+				</Center>
 
 				<Row style={{textAlign:'center', margin: 'auto', width: '75%', marginTop: '40px'}}>
 					<Col l={12} >
